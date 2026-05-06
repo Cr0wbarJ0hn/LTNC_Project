f7c0cadf35b019fd533cb496df3c90c65fc9a203
daodinhtien007-rgb <daodinhtien007@gmail.com>
2026-05-06 23:59:27 +0700

    commit Entity.java

---
+++ src/main/java/com/auction/shared/model/Entity.java
@@ -0,0 +1,26 @@
+package com.auction.shared.model;
+
+public abstract class Entity {
+    protected String id;
+    protected String name;
+
+
+
+    public Entity(String id, String name){
+        this.id=id;
+        this.name=name;
+    }
+    public String getId(){
+        return id;
+    }
+    public void setId(String id){
+        this.id=id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
